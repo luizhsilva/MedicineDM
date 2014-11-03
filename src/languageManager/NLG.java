02d492f80ad46ab067ea71844b7c4b75bc205e5a
luizhsilva <luigi.hss@gmail.com>
2014-11-03 16:24:07 +0100

    add NLG prototype
    NLG with one possible result

--- src/languageManager/NLG.java
+++ src/languageManager/NLG.java
@@ -1,5 +1,29 @@
 package languageManager;
 
+import dm.CommandState;
+import dm.DialogManager;;
+
+/**
+ * 
+ * @author luizhsilva
+ *
+ */
 public class NLG {
+	
+	public NLG() {
+		
+	}
+	
+	/**
+	 * TODO
+	 * Prototype
+	 * @return
+	 */
+	public static String getResult() {
+		if (DialogManager.getDM().getCurrentState() == CommandState.ZEIGMIR) {
+			return "Zeigen --";
+		}
+		return "Nicht verstanden";
+	}
 
 }
