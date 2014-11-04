65546ee518b25f393a1cdb839d1707a1f9c48305
luizhsilva <luigi.hss@gmail.com>
2014-11-04 16:10:20 +0100

    update NLG and prompt

--- runnables/README.txt
+++ runnables/README.txt
@@ -1,4 +1,4 @@
-						Medicine Dialog Manager
+                             Medicine Dialog Manager
 		
 	What is it?
 	-----------
@@ -6,14 +6,16 @@
 	The Medicine Dialog Manager is a software implemented to assist
 	the execution of medical operations. It is able to understand 
 	certain commands and execute them in order to provide the doctor
-	and his/her assistants information about the pacient hands-free.
+	and his/her assistants information about the patient hands-free.
 	
 	The Current Version
 	-------------------
 	
 	The current version of the software can understand few commands
-	given in text form through a keyboard, simply returning a feeback
+	given in text form through a keyboard, simply returning a feedback
 	of what would happen. It does not support Error Handling.
+	
+	Small issues fixed and user prompt was added.
 	
 	Documentation
 	-------------
@@ -23,25 +25,25 @@
 	How to use
 	----------
 	
-	The file medicinedm_alhpa1.jar can be start through command line
-	with 'java -jar medicinedm_alpha2.jar'. It will be promptly wait for
-	a user input, answering in accordance. To programm can be finished
+	The file medicinedm_alhpa3.jar can be start through command line
+	with 'java -jar medicinedm_alpha3.jar'. It will be promptly wait for
+	a user input, answering in accordance. To program can be finished
 	with the input 'shut down'.
 	
 	Commands understood (both upper and lower case accepted):
-	- Zeig mir
-	- Wo ist
-	- Starte
-	- Beende
-	- Blende aus
-	- Blende ein
+	- Zeig mir (+ anatomic structure)
+	- Wo ist (+ anatomic structure)
+	- Starte (+ Process)
+	- Beende (+ Process)
+	- Blende aus (+ Object/Process/Anatomic Structure)
+	- Blende ein (+ Object/Process/Anatomic Structure)
 	- Messen
 	
 	The commands will expect an object. The object can be either an
-	anatomic structure or a process, or even other system elemtents such
+	anatomic structure or a process, or even other system elements such
 	Time.
 	
-	Anatomic Structures/ Objects:
+	Anatomic Structures:
 	- Ureter
 	- Milz
 	- Niere
@@ -56,9 +58,11 @@
 	- Resektionslinie
 	- Gefäßbaum
 	
+	Objects:
 	- Virtuelles Modell
 	- 3D-Rekonstruktion
 	- Registrierung
+	- Registrierungsfehler
 	- Bauchdecke
 	- Endoskopposition
 	- Verschiebungsfeld
@@ -66,6 +70,13 @@
 	- Uhrzeit
 	- Darmlänge
 	- Darmvermessung
+
+	Processes:
+	- Navigation
+	- Virtuelles Modell/ Virtuelle Modell
+	- 3D-Rekonstruktion/ 3D Rekonstruktion/ 3 D Rekonstruktion
+	- Lehrmodus
+	- CT-Modus/ CT Modus
 	
 	Contact
 	-------
--- src/languageManager/NLG.java
+++ src/languageManager/NLG.java
@@ -23,52 +23,61 @@ public class NLG {
 	 */
 	public static String getResult(KeywordCapsule capsule) {
 		CommandState currentState = DialogManager.getDM().getCurrentState();
+		
+		//Zeig mir
 		if (currentState == CommandState.ZEIGMIR) {
 			if (capsule.getAnatStructKeywords().size() != 0) {
+				//Zeig mir Tumor in Organ
 				if(capsule.getAnatStructKeywords().contains("Tumor")) {
 					if(capsule.getAnatStructKeywords().size() > 1) {
-						return "ÝÝ Zeige Tumor in " + capsule.getAnatStructKeywords().get(0) + " ÜÜ";
+						return ">> Zeige Tumor in " + capsule.getAnatStructKeywords().get(0) + " <<";
 					}
 					else {
 						return "xx Richtiger Satzbau: Zeig mir Tumor in + anatomische Struktur xx";
 					}
 				}
-				return "ÝÝ Zeige " + capsule.getAnatStructKeywords().get(0) + " ÜÜ";
+				return ">> Zeige " + capsule.getAnatStructKeywords().get(0) + " <<";
 			} else {
-				return "xx Richtiger Satzbau: Zeig mir + anatomische Struktur xx";
+				return "-- Richtiger Satzbau: Zeig mir + anatomische Struktur --";
 			}
+		//Wo ist
 		} else if (currentState == CommandState.WOIST) {
 			if (capsule.getAnatStructKeywords().size() != 0) {
-				return "ÝÝ Hier ist " + capsule.getAnatStructKeywords().get(0) + " ÜÜ";
+				return ">> Hier ist " + capsule.getAnatStructKeywords().get(0) + " <<";
 			} else {
-				return "xx Richtiger Satzbau: Wo ist + anatomische Struktur xx";
+				return "-- Richtiger Satzbau: Wo ist + anatomische Struktur --";
 			}
+		//Blende aus
 		} else if (currentState == CommandState.BLENDEAUS) {
 			if (capsule.getObjectsKeywords().size() != 0) {
-				return capsule.getObjectsKeywords().get(0) + " ausgeblendet" + " ÜÜ";
+				return ">> " + capsule.getObjectsKeywords().get(0) + " wird ausgeblendet" + " <<";
 			} else if (capsule.getAnatStructKeywords().size() != 0) {
-				return capsule.getAnatStructKeywords().get(0) + " ausgeblendet" + " ÜÜ";
+				return ">> " + capsule.getAnatStructKeywords().get(0) + " wird ausgeblendet" + " <<";
 			}
+		//Blende ein
 		} else if (currentState == CommandState.BLENDEEIN) {
 			if (capsule.getObjectsKeywords().size() != 0) {
-				return "ÝÝ "+ capsule.getObjectsKeywords().get(0) + " eingeblendet" + " ÜÜ";
+				return ">> " + capsule.getObjectsKeywords().get(0) + " wird eingeblendet" + " <<";
 			} else if (capsule.getAnatStructKeywords().size() != 0) {
-				return "ÝÝ "+ capsule.getAnatStructKeywords().get(0) + " eingeblendet" + " ÜÜ";
+				return ">> " + capsule.getAnatStructKeywords().get(0) + " wird eingeblendet" + " <<";
 			}
+		//Messen
 		} else if (currentState == CommandState.MESSEN) {
 			//TODO check if Darmvermessung began
 			return "Messe";
+		//Starte
 		} else if (currentState == CommandState.STARTE) {
 			if (capsule.getProcessKeywords().size() != 0) {
-				return "ÝÝ "+ capsule.getProcessKeywords().get(0) + " wird gestartet." + " ÜÜ";
-			} else return "xx Richtiger Satzbau: Starte + Prozessname xx";
+				return ">> " + capsule.getProcessKeywords().get(0) + " wird gestartet." + " <<";
+			} else return "-- Richtiger Satzbau: Starte + Prozessname --";
+		//Beende
 		} else if (currentState == CommandState.BEENDE) {
 			if (capsule.getProcessKeywords().size() != 0) {
-				return "ÝÝ "+ capsule.getProcessKeywords().get(0) + " wird beendet." + " ÜÜ";
-			} else return "xx Richtiger Satzbau: Beende + Prozessname xx";
+				return ">> " + capsule.getProcessKeywords().get(0) + " wird beendet." + " <<";
+			} else return "-- Richtiger Satzbau: Beende + Prozessname --";
 		};
 		
-		return "xx Nicht verstanden xx";
+		return "-- Nicht verstanden --";
 	}
 
 }
